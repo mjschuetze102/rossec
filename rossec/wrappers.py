2675dcae7fb678b8bab7df303322f091e3c78730
Michael Schuetze <mjschuetze102@gmail.com>
2020-04-15 16:33:54 -0700

    Got the wrapper working for a String

--- rossec/wrappers.py
+++ rossec/wrappers.py
@@ -1,38 +1,52 @@
 #!/usr/bin/env python
+import json
 import rospy
-from rossec.msg import String
+from rossec.msg import SecuredMessage
+from rossec.security import AESEncryption
 
 
-class Publisher:
+class Encryptable(object):
+    """
+    Initializes the security aspect of the wrappers
+    """
+
+    def __init__(self):
+        self.AES = AESEncryption(bytes("12345678901234567890123456789012"))
+
+class Publisher(Encryptable):
     """
     Security wrapper for the Publisher class
     """
 
     def __init__(self, *args, **kwargs):
-        data_class = args[1]
-        print("Data Class: ", data_class)
-        args = args[:1] + (String,) + args[2:]
-        print("New Data Class: ", args[1])
+        Encryptable.__init__(self)
+        self.data_class = args[1]
+        args = args[:1] + (SecuredMessage,) + args[2:]
         self.pub = rospy.Publisher(*args, **kwargs)
 
     def publish(self, *args, **kwargs):
-        self.pub.publish(*args, **kwargs)
+        msg = SecuredMessage()
+        msg.MessageType = str(self.data_class)
+        msg.MessageContent = self.AES.encode(json.dumps(args[0]))
+        self.pub.publish(msg)
 
-class Subscriber:
+class Subscriber(Encryptable):
     """
     Security wrapper for the Subscriber class
     """
 
     def __init__(self, *args, **kwargs):
-        data_class = args[1]
-        callback = args[2] if len(args) > 2 else kwargs.get('callback', None)
-        callback_args = args[3] if len(args) > 3 else kwargs.get('callback_args', None)
-        print("Data Class: ", data_class)
-        print("Callback: ", callback)
-        print("Callback Args: ", callback_args)
-        args = args[:1] + (String,) + args[2:]
-        print("New Data Class: ", args[1])
+        Encryptable.__init__(self)
+        self.data_class = args[1]
+        self.callback = args[2] if len(args) > 2 else kwargs.get('callback', None)
+        self.callback_args = args[3] if len(args) > 3 else kwargs.get('callback_args', None)
+        args = args[:1] + (SecuredMessage,) + args[3:]
+        kwargs['callback'] = self.__callback__
         self.sub = rospy.Subscriber(*args, **kwargs)
+
+    def __callback__(self, data):
+        parsed = json.loads(self.AES.decode(data.MessageContent))
+        self.callback(self.data_class(parsed))
 
     def unregister(self):
         self.sub.unregister()
