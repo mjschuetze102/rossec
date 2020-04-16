3b076d4b3a605fc94b8e59e162207b5175179b30
Michael Schuetze <mjschuetze102@gmail.com>
2020-04-15 21:24:37 -0700

    Modified wrappers.py to accept more types of messages

--- rossec/wrappers.py
+++ rossec/wrappers.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
-import json
 import rospy
+from rospy.msg import args_kwds_to_message
 from rossec.msg import SecuredMessage
 from rossec.security import AESEncryption
+
+try:     # Python 2.x
+    from cStringIO import StringIO
+except:  # Python 3.x
+    from io import StringIO
 
 
 class Encryptable(object):
@@ -19,15 +24,30 @@ class Publisher(Encryptable):
     """
 
     def __init__(self, *args, **kwargs):
-        Encryptable.__init__(self)
-        self.data_class = args[1]
-        args = args[:1] + (SecuredMessage,) + args[2:]
+        super(Publisher, self).__init__()
+        self.name = args[0] if len(args) > 0 else kwargs.get('name', None)
+        self.data_class = args[1] if len(args) > 1 else kwargs.get('data_class', None)
+        
+        # Change values in args list as needed
+        args = args[2:]
+        kwargs['name'] = self.name
+        kwargs['data_class'] = SecuredMessage
         self.pub = rospy.Publisher(*args, **kwargs)
 
     def publish(self, *args, **kwargs):
+        # Convert arguments into an instance of provided data_class
+        orig = args_kwds_to_message(self.data_class, args, kwargs)
+
+        # Get the attribute information
+        buff = StringIO()
+        orig.serialize(buff)
+        contents = buff.getvalue()
+        buff.close()
+        
+        # Create the new message to send
         msg = SecuredMessage()
         msg.MessageType = str(self.data_class)
-        msg.MessageContent = self.AES.encode(json.dumps(args[0]))
+        msg.MessageContent = self.AES.encode(contents)
         self.pub.publish(msg)
 
 class Subscriber(Encryptable):
@@ -36,17 +56,24 @@ class Subscriber(Encryptable):
     """
 
     def __init__(self, *args, **kwargs):
-        Encryptable.__init__(self)
-        self.data_class = args[1]
+        super(Subscriber, self).__init__()
+        self.name = args[0] if len(args) > 0 else kwargs.get('name', None)
+        self.data_class = args[1] if len(args) > 1 else kwargs.get('data_class', None)
         self.callback = args[2] if len(args) > 2 else kwargs.get('callback', None)
         self.callback_args = args[3] if len(args) > 3 else kwargs.get('callback_args', None)
-        args = args[:1] + (SecuredMessage,) + args[3:]
+        
+        # Change values in args list as needed
+        args = args[3:]
+        kwargs['name'] = self.name
+        kwargs['data_class'] = SecuredMessage
         kwargs['callback'] = self.__callback__
         self.sub = rospy.Subscriber(*args, **kwargs)
 
     def __callback__(self, data):
-        parsed = json.loads(self.AES.decode(data.MessageContent))
-        self.callback(self.data_class(parsed))
+        # Create the original message from the encrypted data
+        obj = self.data_class()
+        obj.deserialize(self.AES.decode(data.MessageContent))
+        self.callback(obj)
 
     def unregister(self):
         self.sub.unregister()
