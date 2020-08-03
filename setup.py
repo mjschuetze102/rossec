78e9fec1cbb2d80d30b5a1ad5afde0b6eda21137
Michael Schuetze <mjschuetze102@gmail.com>
2020-08-02 18:05:47 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -16,9 +16,20 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    python_requires='>=2.7',
+    install_requires=[
+        'pycryptodome',
+    ],
+    python_requires='>=2.7, !=3.0.*, !=3.1*, !=3.2*, !=3.3*, !=3.4*, !=3.5*',
 )
 
